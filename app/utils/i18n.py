e17f6a0b9779ce6fb7ac44860fb421bf616ac7f0
Lucas Lage <ufmg.lucaslage@pythonanalytics-gsi-des01.mpmg.mp.br>
2025-03-12 01:01:40 -0300

    Fix i18n files

--- app/utils/i18n.py
+++ app/utils/i18n.py
@@ -6,17 +6,17 @@ _translation = None
 DEFAULT_LANGUAGE = "en"
 SUPPORTED_LANGUAGE = ["pt", "en"]
 
-_path = (pathlib.Path(__file__).parent.parent / "i18n" / "locales").resolve()
 
+pathlib.Path(__file__).parent.resolve()
+
+_path = (pathlib.Path(__file__).parent.parent / "i18n" / "locales").resolve()
 
 def active_translation(lang: str):
     global _default_lang
     global _translation
-
+    
     _default_lang = DEFAULT_LANGUAGE if lang not in SUPPORTED_LANGUAGE else lang
-    _translation = gt.translation(
-        "messages", localedir=_path, languages=[_default_lang]
-    )
+    _translation = gt.translation("messages", localedir=_path, languages=[_default_lang])
 
 
 def gettext(message: str) -> str:
