0ca9524e96542b877848e4edf584708736bafa5c
Walter dos Santos Filho <waltersf@gmail.com>
2025-03-21 11:43:29 -0300

    Fix tests

--- app/database.py
+++ app/database.py
@@ -18,29 +18,30 @@ metadata = MetaData(naming_convention=DEFAULT_NAMING_CONVENTION)
 class Base(DeclarativeBase):
     metadata = metadata
 
+if 'sqlite' in DATABASE_URL:
+    DATABASE_URL = "sqlite+asyncsqlite:///./test.db"
 
-if 'sqlite' not in DATABASE_URL:
-    # Create the engine
-    engine = create_async_engine(
-        DATABASE_URL, echo=True, pool_size=10, max_overflow=20
-    )
+# Create the engine
+engine = create_async_engine(
+    DATABASE_URL, echo=True, pool_size=10, max_overflow=20
+)
 
-    # Create a configured "Session" class
-    SessionLocal = sessionmaker(
-        autocommit=False,
-        autoflush=False,
-        bind=engine,
-        class_=AsyncSession,
-        expire_on_commit=False,
-    )
+# Create a configured "Session" class
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine, # type: ignore
+    class_=AsyncSession,
+    expire_on_commit=False,
+) # type: ignore
 
 
-    # Create a session factory
-    AsyncSessionLocal = sessionmaker(
-        bind=engine, class_=AsyncSession, expire_on_commit=False
-    )
+# Create a session factory
+AsyncSessionLocal = sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False # type: ignore
+) # type: ignore
 
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
-    async with AsyncSessionLocal() as session:
+    async with AsyncSessionLocal() as session: # type: ignore
         yield session
