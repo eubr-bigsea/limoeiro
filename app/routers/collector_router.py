f858d451ceb6369b9d8ca24e40a2bc20b44ecc16
Lucas Lage <ufmg.lucaslage@pythonanalytics-gsi-des01.mpmg.mp.br>
2025-05-07 15:20:24 -0300

    Test end point with JWT Token

--- app/routers/collector_router.py
+++ app/routers/collector_router.py
@@ -3,8 +3,12 @@ import logging
 from fastapi import (
     APIRouter,
     BackgroundTasks,
+    Request,
+    Header
 )
-
+from typing import Optional
+import base64
+import jwt
 from app.collector.data_collection_engine import DataCollectionEngine
 
 router = APIRouter()
@@ -19,3 +23,21 @@ async def collect_data(
     if background_tasks is not None:
         background_tasks.add_task(engine.execute_engine)
     return True
+
+
+@router.get("/secure-endpoint")
+async def secure_endpoint(request: Request, x_jwt_assertion: Optional[str] = Header(None)):
+    if not x_jwt_assertion:
+        return {"error": "X-JWT-Assertion header missing"}
+
+    try:
+        # Base64 decode the JWT string from the header
+        jwt_bytes = base64.b64decode(x_jwt_assertion)
+        jwt_str = jwt_bytes.decode('utf-8')
+
+        # Decode the JWT payload without verifying signature
+        decoded_token = jwt.decode(jwt_str, options={"verify_signature": False})
+        return {"decoded_jwt": decoded_token}
+
+    except Exception as e:
+        return {"error": str(e)}
