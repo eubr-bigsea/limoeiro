db497c182fc6fc2ab2ac65d79ba1d875e9c6eb7e
Lucas Lage <ufmg.lucaslage@pythonanalytics-gsi-des01.mpmg.mp.br>
2025-05-25 19:01:10 -0300

    Fixing sample collectors

--- app/collector/mongo_collector.py
+++ app/collector/mongo_collector.py
@@ -17,7 +17,6 @@ from app.schemas import (
     TableColumnCreateSchema,
     DatabaseTableSampleCreateSchema,
 )
-from datetime import datetime
 
 class MongoCollector(Collector):
     """Class to implement methods, to collect data in Mongo."""
@@ -150,7 +149,7 @@ class MongoCollector(Collector):
         content = list(content)
 
         return DatabaseTableSampleCreateSchema(
-                                sample_date=datetime.now(),
+                                sample_date=datetime.datetime.now(),
                                 content=content,
                                 is_visible=True,
                                 database_table_id=DEFAULT_UUID,
